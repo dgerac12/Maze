50827b0e7cffbc288b04ef3a21129d1ee90beabd
dgerac12 <dgeraci@hotmail.com>
2019-07-17 23:58:15 -0400

    second commit TIER 2 completed

---
+++ Assets/Scripts/EnemyMoveReverse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveReverse : MonoBehaviour
+{
+    Vector3 pointA = new Vector3(0f, 0f, 0f);
+    Vector3 pointB = new Vector3(0f, -2f, 0f);
+    public float speed;
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time * speed, 1));
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
+public GameObject camera;
 private Rigidbody2D rb2d;
 
+private Vector2 charScale;
+private float charScaleX;
 public float speed;
 
+public AudioClip pickupSound;
+public AudioSource pickupSource;
+public AudioClip enemySound;
+public AudioSource enemySource;
+public AudioClip bgMusic;
+public AudioSource bgSource;
+public AudioSource winSource;
+public AudioClip winMusic;
+
+public Text scoreText;
+public Text livesText;
+public Text winText;
+public Text overText;
+
+private int score;
+private int lives;
 public float jumpForce;
 
     void Start()
     {
+        pickupSource.clip = pickupSound;
+        enemySource.clip = enemySound;
+        bgSource.clip = bgMusic;
+        winSource.clip = winMusic;
+        bgSource.Play();
         rb2d = GetComponent<Rigidbody2D>();
+
+        score = 0;
+        SetScoreText();
+        winText.text = "";
+        overText.text = "";
+
+        lives = 3;
+        SetLivesText();
     }
 
     void Update()
     {
         if(Input.GetKey("escape"))
+        {
             Application.Quit();
+        }
+        if (lives <= 1)
+        {
+            livesText.color = new Color(168f/255f, 23f/255f, 23f/255f);
+        }
+        if (lives == 0)
+        { 
+            Destroy(this);
+            overText.text = "Game Over";
+        }
+
     }
     void FixedUpdate()
     {
@@ -28,8 +73,45 @@ public float jumpForce;
         Vector2 movement = new Vector2(moveHorizontal, 0);
 
         rb2d.AddForce(movement * speed);
+
+        if (Input.GetKey("right")) {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
     }
 
+    void OnTriggerEnter2D(Collider2D other) 
+        {
+        if (other.gameObject.CompareTag("Pickup")) 
+            {
+                other.gameObject.SetActive (false);
+                pickupSource.Play();
+                score = score + 1;
+                SetScoreText();
+            }
+
+        if (score == 4)
+            {
+                transform.position = new Vector3(16f, -2.2f, 0f);
+                camera.transform.position = new Vector3(23.75f, 0f, -10f);
+                livesText.color = Color.white;
+                lives = 3;
+                SetLivesText();
+            }
+
+        else if (other.gameObject.CompareTag("Enemy"))
+            {
+                other.gameObject.SetActive(false);
+                enemySource.Play();
+                lives = lives - 1;
+                SetLivesText ();
+            }
+
+        }
     void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.collider.tag == "Ground")
@@ -39,5 +121,23 @@ public float jumpForce;
                 rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
         }
+    }
+
+
+    void SetScoreText ()
+    {
+        scoreText.text = "Score: " + score.ToString ();
+            if (score >= 8) {
+            bgSource.Stop();
+            winSource.Play();
+            winText.text = "You Win!";
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach(GameObject enemy in enemies)
+            GameObject.Destroy(enemy);
+            }
+    }
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
     }
 }
